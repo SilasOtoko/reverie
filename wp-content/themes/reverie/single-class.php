35f13df5eecbcf4dc9dae349a58824eff237f3a3
Josiah Schaefer <josiahschaefer@Josiahs-MacBook-Pro.local>
2016-09-26 11:32:55 -0400

    trying to figure out classes, structure and function

--- wp-content/themes/reverie/single-class.php
+++ wp-content/themes/reverie/single-class.php
@@ -11,13 +11,28 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="medium-wrapper">
+			<div id="single-class-page" class="medium-wrapper">
 
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	        <h2><?php the_title(); ?></h2>
+	        <div class="page-title">
+		        <h1><?php the_title(); ?></h1>
+		        <div class="fancy-underline">
+		          <span class="line"></span>
+		          <img class="fancy-swirl" src="<?php bloginfo( 'template_directory' ); ?>/img/art.svg" alt="fancy swirl">
+		          <span class="line"></span>
+		        </div>
+						<h4><em>*Registration Required for all events.</em></h4>
+		      </div>
 
           <?php the_field( 'scheduled_classes' ); ?>
+
+					<?php
+					$event = get_the_title()->_event_title; ?>
+
+					<h1><?php echo $id; ?></h1>
+
+					<?php echo EM_Events::output(array('tag'=>$event,  'limit'=>10, 'pagination'=>1)); ?>
 
 	      <?php endwhile; else : ?>
 
